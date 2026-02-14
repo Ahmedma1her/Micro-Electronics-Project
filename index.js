2fbef979d0aa936a52b660b4ea5afba484d5ccc4
Ahmed Maher <AhmedMa1her@gmail.com>
2026-02-14 16:30:22 +0200

    user registeration added

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 require ("dotenv").config();
 const express = require("express");
-
+const bcrypt=require("bcrypt")
 const app = express();
 const port = process.env.PORT || 3000;
 app.use(express.json());
@@ -19,7 +19,35 @@ async function Db_connection() {
 }
 Db_connection();
 
+const User = require("./models/User");
 
+app. post('/api/register', async (req, res) =>{
+    try {
+        //get data
+        const { username, email, password , role} = req.body;
+        // validate data
+        if(!username|| !email || !password) return res.status(400).json({msg:"missing data"});
+
+        const existUser= await User.findOne({email});
+        if(existUser)return res.status(400).json({msg:"email is already used"})
+        // create user
+const hashPassword = await bcrypt.hash(password, 10)
+        const user = await User.create({
+            username,
+            email,
+            password: hashPassword,
+            role
+        })
+        res.status(201).json({
+            msg:"user created",
+            data: user})
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({msg:"server error"})
+        
+    }
+}  
+)
 
 
 
