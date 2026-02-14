b906c82e0d7be69c03048f6eaa567afaa7a5cab2
Ahmed Maher <AhmedMa1her@gmail.com>
2026-02-14 18:55:25 +0200

    2nd phase semi finished

--- index.js
+++ index.js
@@ -19,6 +19,9 @@ async function Db_connection() {
 }
 Db_connection();
 
+//////////////////////////////
+///////////phase 1///////////
+/////////////////////////////
 const User = require("./models/User");
 
 app.post('/api/register', async (req, res) =>{
@@ -70,6 +73,74 @@ app.post('/api/login', async (req, res) =>{
         res.status(500).json({msg:"server error"})
     }
 })
+
+
+
+//////////////////////////////
+///////////phase 2///////////
+/////////////////////////////
+
+
+const Product = require("./models/Product");
+
+// create product
+app.post('/api/products', async (req, res) => {
+    try {
+        const product = await Product.create(req.body);
+        res.status(201).json({msg:"product created", data: product})
+    } catch (error) {
+        console.log(error);
+        res.status(400).json({msg:"product creation failed"})
+    }
+})
+// manager can update product when needed
+app.put('/api/products/:id', async (req, res) => {
+    try {
+        const {id} = req.params;
+        const product = await Product.findByIdAndUpdate(id, req.body, {new: true});
+        if(!product) return res.status(404).json({msg:"product not found"});
+        res.status(200).json({msg:"product updated", data: product})
+    } catch (error) {
+        console.log(error);
+        res.status(400).json({msg:"product update failed"})
+    }   
+})
+// manager can delete product when needed
+app.delete('/api/products/:id', async (req, res) => {
+    try {
+        const {id} = req.params;
+        const product = await Product.findByIdAndDelete(id);
+        if(!product) return res.status(404).json({msg:"product not found"});
+        res.status(200).json({msg:"product deleted"})
+    } catch (error) {
+        console.log(error);
+        res.status(400).json({msg:"product deletion failed"})
+    }
+})
+
+
+//user can view all products
+
+// user can view specific product details
+
+app.get('/api/products', async (req, res) => {
+    try {
+        const { productName } = req.query;
+
+        let products;
+
+        if (productName) {products = await Product.find({productName: productName  });
+        } else { products = await Product.find();  }
+        res.status(200).json({
+            msg: "products retrieved",
+            data: products
+        });
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ msg: "server error" });
+    }
+});
 
 
 
